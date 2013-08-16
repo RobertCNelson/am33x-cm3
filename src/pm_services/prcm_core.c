fbeb6d532d6edb7fdcfe1e64a601dbfa65bc445b
Russ Dill <Russ.Dill@ti.com>
2013-08-16 00:39:58 -0700

    CM3: Declare missing type for _next_pd_*_stctrl_val functions.
    Signed-off-by: Russ Dill <Russ.Dill@ti.com>

--- src/pm_services/prcm_core.c
+++ src/pm_services/prcm_core.c
@@ -621,7 +621,7 @@ int mpu_powerst_change(int val, int var)
 	return var;
 }
 
-static int _next_pd_per_stctrl_val(state)
+static int _next_pd_per_stctrl_val(int state)
 {
 	int v = 0;
 
@@ -652,7 +652,7 @@ int get_pd_per_stctrl_val(int state)
 	return _next_pd_per_stctrl_val(state);
 }
 
-static int _next_pd_mpu_stctrl_val(state)
+static int _next_pd_mpu_stctrl_val(int state)
 {
 	int v = 0;
 
