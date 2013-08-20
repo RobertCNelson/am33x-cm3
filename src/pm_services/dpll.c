ddbd8f5efa0a120193f42709d9fcda2341535123
Russ Dill <Russ.Dill@ti.com>
2013-08-19 21:24:44 -0700

    CM3: Rework dpll struct to prepare for am335x/am43xx split.
    Combine the information on the different parts of the DPLLs into
    one struct. This will make the am335x/am43xx split easier.
    
    Signed-off-by: Russ Dill <Russ.Dill@ti.com>

--- src/pm_services/dpll.c
+++ src/pm_services/dpll.c
@@ -18,23 +18,27 @@
 #include <system_am335.h>
 #include <dpll.h>
 
-struct dpll_pd_data {
-	int dpll_reg;
-	int sw_ctrl_dpll_bit;
-	int isoscan_bit;
-	int ret_bit;
-	int reset_bit;
-	int iso_bit;
-	int pgoodin_bit;
-	int ponin_bit;
-	int dpll_pwr_sw_status_reg;
-	int pgoodout_status_bit;
-	int ponout_status_bit;
+struct dpll_regs {
+	unsigned int dpll_pwr_sw_ctrl_reg;
+	unsigned int sw_ctrl_dpll_bit;
+	unsigned int isoscan_bit;
+	unsigned int ret_bit;
+	unsigned int reset_bit;
+	unsigned int iso_bit;
+	unsigned int pgoodin_bit;
+	unsigned int ponin_bit;
+
+	unsigned int dpll_pwr_sw_status_reg;
+	unsigned int pgoodout_status_bit;
+	unsigned int ponout_status_bit;
+
+	unsigned int clkmode_reg;
+	unsigned int idlest_reg;
 };
 
-static const struct dpll_pd_data dpll_data[DPLL_COUNT]  = {
+static const struct dpll_regs dpll_regs[DPLL_COUNT]  = {
 	[DPLL_PER] = {
-		.dpll_reg		= DPLL_PWR_SW_CTRL,
+		.dpll_pwr_sw_ctrl_reg	= DPLL_PWR_SW_CTRL,
 		.sw_ctrl_dpll_bit	= SW_CTRL_PER_DPLL,
 		.isoscan_bit		= ISOSCAN_PER,
 		.ret_bit		= RET_PER,
@@ -45,9 +49,11 @@ static const struct dpll_pd_data dpll_data[DPLL_COUNT]  = {
 		.dpll_pwr_sw_status_reg	= DPLL_PWR_SW_STATUS,
 		.pgoodout_status_bit	= PGOODOUT_PER_STATUS,
 		.ponout_status_bit	= PONOUT_PER_STATUS,
+		.clkmode_reg		= AM335X_CM_CLKMODE_DPLL_PER,
+		.idlest_reg		= AM335X_CM_IDLEST_DPLL_PER,
 	},
 	[DPLL_DISP] = {
-		.dpll_reg		= DPLL_PWR_SW_CTRL,
+		.dpll_pwr_sw_ctrl_reg	= DPLL_PWR_SW_CTRL,
 		.sw_ctrl_dpll_bit	= SW_CTRL_DISP_DPLL,
 		.isoscan_bit		= ISOSCAN_DISP,
 		.ret_bit		= RET_DISP,
@@ -58,9 +64,11 @@ static const struct dpll_pd_data dpll_data[DPLL_COUNT]  = {
 		.dpll_pwr_sw_status_reg	= DPLL_PWR_SW_STATUS,
 		.pgoodout_status_bit	= PGOODOUT_DISP_STATUS,
 		.ponout_status_bit	= PONOUT_DISP_STATUS,
+		.clkmode_reg		= AM335X_CM_CLKMODE_DPLL_DISP,
+		.idlest_reg		= AM335X_CM_IDLEST_DPLL_DISP,
 	},
 	[DPLL_DDR] = {
-		.dpll_reg		= DPLL_PWR_SW_CTRL,
+		.dpll_pwr_sw_ctrl_reg	= DPLL_PWR_SW_CTRL,
 		.sw_ctrl_dpll_bit	= SW_CTRL_DDR_DPLL,
 		.isoscan_bit		= ISOSCAN_DDR,
 		.ret_bit		= RET_DDR,
@@ -71,8 +79,20 @@ static const struct dpll_pd_data dpll_data[DPLL_COUNT]  = {
 		.dpll_pwr_sw_status_reg	= DPLL_PWR_SW_STATUS,
 		.pgoodout_status_bit	= PGOODOUT_DDR_STATUS,
 		.ponout_status_bit	= PONOUT_DDR_STATUS,
+		.clkmode_reg		= AM335X_CM_CLKMODE_DPLL_DDR,
+		.idlest_reg		= AM335X_CM_IDLEST_DPLL_DDR,
+	},
+	[DPLL_MPU] = {
+		.clkmode_reg		= AM335X_CM_CLKMODE_DPLL_MPU,
+		.idlest_reg		= AM335X_CM_IDLEST_DPLL_MPU,
+	},
+	[DPLL_CORE] = {
+		.clkmode_reg		= AM335X_CM_CLKMODE_DPLL_CORE,
+		.idlest_reg		= AM335X_CM_IDLEST_DPLL_CORE,
 	},
 };
+
+static unsigned int pll_mode[DPLL_COUNT];
 
 /* DPLL power-down Sequence PG 2.x */
 static void dpll_power_down(enum dpll_id dpll)
@@ -80,39 +100,39 @@ static void dpll_power_down(enum dpll_id dpll)
 	unsigned int var;
 
 	/* Configure bit to select Control module selection for DPLL */
-	var = __raw_readl(dpll_data[dpll].dpll_reg);
-	var |= dpll_data[dpll].sw_ctrl_dpll_bit;
-	__raw_writel(var, dpll_data[dpll].dpll_reg);
+	var = __raw_readl(dpll_regs[dpll].dpll_pwr_sw_ctrl_reg);
+	var |= dpll_regs[dpll].sw_ctrl_dpll_bit;
+	__raw_writel(var, dpll_regs[dpll].dpll_pwr_sw_ctrl_reg);
 
 	/* Assert ISO bit high */
-	var = __raw_readl(dpll_data[dpll].dpll_reg);
-	var |= dpll_data[dpll].iso_bit;
-	__raw_writel(var, dpll_data[dpll].dpll_reg);
+	var = __raw_readl(dpll_regs[dpll].dpll_pwr_sw_ctrl_reg);
+	var |= dpll_regs[dpll].iso_bit;
+	__raw_writel(var, dpll_regs[dpll].dpll_pwr_sw_ctrl_reg);
 
 	/* ISO_SCAN, RET should be asserted high */
-	var = __raw_readl(dpll_data[dpll].dpll_reg);
-	var |= (dpll_data[dpll].isoscan_bit | dpll_data[dpll].ret_bit);
-	__raw_writel(var, dpll_data[dpll].dpll_reg);
+	var = __raw_readl(dpll_regs[dpll].dpll_pwr_sw_ctrl_reg);
+	var |= (dpll_regs[dpll].isoscan_bit | dpll_regs[dpll].ret_bit);
+	__raw_writel(var, dpll_regs[dpll].dpll_pwr_sw_ctrl_reg);
 
 	/* Assert DPLL reset to 1 */
-	var = __raw_readl(dpll_data[dpll].dpll_reg);
-	var |= dpll_data[dpll].reset_bit;
-	__raw_writel(var, dpll_data[dpll].dpll_reg);
+	var = __raw_readl(dpll_regs[dpll].dpll_pwr_sw_ctrl_reg);
+	var |= dpll_regs[dpll].reset_bit;
+	__raw_writel(var, dpll_regs[dpll].dpll_pwr_sw_ctrl_reg);
 
 	/* PGOODIN signal is de-asserted low */
-	var = __raw_readl(dpll_data[dpll].dpll_reg);
-	var &= ~dpll_data[dpll].pgoodin_bit;
-	__raw_writel(var, dpll_data[dpll].dpll_reg);
+	var = __raw_readl(dpll_regs[dpll].dpll_pwr_sw_ctrl_reg);
+	var &= ~dpll_regs[dpll].pgoodin_bit;
+	__raw_writel(var, dpll_regs[dpll].dpll_pwr_sw_ctrl_reg);
 
 	/* PONIN signal is de-asserted low */
-	var = __raw_readl(dpll_data[dpll].dpll_reg);
-	var &= ~dpll_data[dpll].ponin_bit;
-	__raw_writel(var, dpll_data[dpll].dpll_reg);
+	var = __raw_readl(dpll_regs[dpll].dpll_pwr_sw_ctrl_reg);
+	var &= ~dpll_regs[dpll].ponin_bit;
+	__raw_writel(var, dpll_regs[dpll].dpll_pwr_sw_ctrl_reg);
 
 	/* Poll for PONOUT and PGOODOUT signal status as 0 */
-	while (__raw_readl(dpll_data[dpll].dpll_pwr_sw_status_reg) &
-			(dpll_data[dpll].pgoodout_status_bit |
-			dpll_data[dpll].ponout_status_bit));
+	while (__raw_readl(dpll_regs[dpll].dpll_pwr_sw_status_reg) &
+			(dpll_regs[dpll].pgoodout_status_bit |
+			dpll_regs[dpll].ponout_status_bit));
 }
 
 /* DPLL Power-up Sequence */
@@ -121,42 +141,42 @@ static void dpll_power_up(enum dpll_id dpll)
 	unsigned int var;
 
 	/* PONIN is asserted high */
-	var = __raw_readl(dpll_data[dpll].dpll_reg);
-	var |= dpll_data[dpll].ponin_bit;
-	__raw_writel(var, dpll_data[dpll].dpll_reg);
+	var = __raw_readl(dpll_regs[dpll].dpll_pwr_sw_ctrl_reg);
+	var |= dpll_regs[dpll].ponin_bit;
+	__raw_writel(var, dpll_regs[dpll].dpll_pwr_sw_ctrl_reg);
 
 	/* Poll for PONOUT to become high  */
-	while (!(__raw_readl(dpll_data[dpll].dpll_pwr_sw_status_reg) &
-				dpll_data[dpll].ponout_status_bit));
+	while (!(__raw_readl(dpll_regs[dpll].dpll_pwr_sw_status_reg) &
+				dpll_regs[dpll].ponout_status_bit));
 
 	/* PGOODIN is asserted high */
-	var = __raw_readl(dpll_data[dpll].dpll_reg);
-	var |= dpll_data[dpll].pgoodin_bit;
-	__raw_writel(var, dpll_data[dpll].dpll_reg);
+	var = __raw_readl(dpll_regs[dpll].dpll_pwr_sw_ctrl_reg);
+	var |= dpll_regs[dpll].pgoodin_bit;
+	__raw_writel(var, dpll_regs[dpll].dpll_pwr_sw_ctrl_reg);
 
 	/* Poll for PGOODOUT to become high */
-	while (!(__raw_readl(dpll_data[dpll].dpll_pwr_sw_status_reg) &
-				dpll_data[dpll].pgoodout_status_bit));
+	while (!(__raw_readl(dpll_regs[dpll].dpll_pwr_sw_status_reg) &
+				dpll_regs[dpll].pgoodout_status_bit));
 
 	/* De-assert DPLL RESET to 0 */
-	var = __raw_readl(dpll_data[dpll].dpll_reg);
-	var &= ~dpll_data[dpll].reset_bit;
-	__raw_writel(var, dpll_data[dpll].dpll_reg);
+	var = __raw_readl(dpll_regs[dpll].dpll_pwr_sw_ctrl_reg);
+	var &= ~dpll_regs[dpll].reset_bit;
+	__raw_writel(var, dpll_regs[dpll].dpll_pwr_sw_ctrl_reg);
 
 	/* ISO_SCAN, RET should be de-asserted low */
-	var = __raw_readl(dpll_data[dpll].dpll_reg);
-	var &= ~(dpll_data[dpll].isoscan_bit | dpll_data[dpll].ret_bit);
-	__raw_writel(var, dpll_data[dpll].dpll_reg);
+	var = __raw_readl(dpll_regs[dpll].dpll_pwr_sw_ctrl_reg);
+	var &= ~(dpll_regs[dpll].isoscan_bit | dpll_regs[dpll].ret_bit);
+	__raw_writel(var, dpll_regs[dpll].dpll_pwr_sw_ctrl_reg);
 
 	/* De-assert ISO signal */
-	var = __raw_readl(dpll_data[dpll].dpll_reg);
-	var &= ~dpll_data[dpll].iso_bit;
-	__raw_writel(var, dpll_data[dpll].dpll_reg);
+	var = __raw_readl(dpll_regs[dpll].dpll_pwr_sw_ctrl_reg);
+	var &= ~dpll_regs[dpll].iso_bit;
+	__raw_writel(var, dpll_regs[dpll].dpll_pwr_sw_ctrl_reg);
 
 	/* Re-Configure bit to select PRCM selection for DPLL */
-	var = __raw_readl(dpll_data[dpll].dpll_reg);
-	var &= ~dpll_data[dpll].sw_ctrl_dpll_bit;
-	__raw_writel(var, dpll_data[dpll].dpll_reg);
+	var = __raw_readl(dpll_regs[dpll].dpll_pwr_sw_ctrl_reg);
+	var &= ~dpll_regs[dpll].sw_ctrl_dpll_bit;
+	__raw_writel(var, dpll_regs[dpll].dpll_pwr_sw_ctrl_reg);
 }
 
 /* DPLL retention update for PG 2.0 */
@@ -183,55 +203,21 @@ void am33xx_power_up_plls(void)
 	}
 }
 
-struct dpll_context {
-	int clk_mode_addr;
-	int idlest_addr;
-	int pll_mode;
-};
-
-struct dpll_context am33xx_dplls[DPLL_COUNT] = {
-	[DPLL_PER] = {
-		.clk_mode_addr	= AM335X_CM_CLKMODE_DPLL_PER,
-		.idlest_addr	= AM335X_CM_IDLEST_DPLL_PER,
-		.pll_mode	= 0,
-	},
-	[DPLL_DISP] = {
-		.clk_mode_addr	= AM335X_CM_CLKMODE_DPLL_DISP,
-		.idlest_addr	= AM335X_CM_IDLEST_DPLL_DISP,
-		.pll_mode	= 0,
-	},
-	[DPLL_DDR] = {
-		.clk_mode_addr	= AM335X_CM_CLKMODE_DPLL_DDR,
-		.idlest_addr	= AM335X_CM_IDLEST_DPLL_DDR,
-		.pll_mode	= 0,
-	},
-	[DPLL_MPU] = {
-		.clk_mode_addr	= AM335X_CM_CLKMODE_DPLL_MPU,
-		.idlest_addr	= AM335X_CM_IDLEST_DPLL_MPU,
-		.pll_mode	= 0,
-	},
-	[DPLL_CORE] = {
-		.clk_mode_addr	= AM335X_CM_CLKMODE_DPLL_CORE,
-		.idlest_addr	= AM335X_CM_IDLEST_DPLL_CORE,
-		.pll_mode	= 0,
-	},
-};
-
 void pll_bypass(enum dpll_id dpll)
 {
-	am33xx_dplls[dpll].pll_mode = __raw_readl(am33xx_dplls[dpll].clk_mode_addr);
-	__raw_writel(((am33xx_dplls[dpll].pll_mode & ~DPLL_EN_MASK) |
-			DPLL_LP_BYP_MODE), am33xx_dplls[dpll].clk_mode_addr);
+	pll_mode[dpll] = __raw_readl(dpll_regs[dpll].clkmode_reg);
+	__raw_writel(((pll_mode[dpll] & ~DPLL_EN_MASK) |
+			DPLL_LP_BYP_MODE), dpll_regs[dpll].clkmode_reg);
 
 	/* Wait for DPLL to enter bypass mode */
-	while (__raw_readl(am33xx_dplls[dpll].idlest_addr));
+	while (__raw_readl(dpll_regs[dpll].idlest_reg));
 }
 
 void pll_lock(enum dpll_id dpll)
 {
-	__raw_writel(am33xx_dplls[dpll].pll_mode, am33xx_dplls[dpll].clk_mode_addr);
+	__raw_writel(pll_mode[dpll], dpll_regs[dpll].clkmode_reg);
 
-	if ((am33xx_dplls[dpll].pll_mode & 0x7) == 0x7)
+	if ((pll_mode[dpll] & 0x7) == 0x7)
 		/* Make sure DPLL Clock is out of Bypass */
-		while (!(__raw_readl(am33xx_dplls[dpll].idlest_addr)));
+		while (!(__raw_readl(dpll_regs[dpll].idlest_reg)));
 }
